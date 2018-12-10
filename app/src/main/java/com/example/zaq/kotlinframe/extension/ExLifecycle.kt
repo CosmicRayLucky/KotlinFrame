a9ef61fec0a9c9774170fa2717be983b52e11786
ZhangAqi <1095187377@qq.com>
2018-12-10 13:55:53 +0800

    添加一些第三方框架util类

--- app/src/main/java/com/example/zaq/kotlinframe/extension/ExLifecycle.kt
+++ app/src/main/java/com/example/zaq/kotlinframe/extension/ExLifecycle.kt
@@ -1,13 +1,14 @@
 package com.example.zaq.kotlinframe.extension
 
-import com.trello.rxlifecycle2.kotlin.bindToLifecycle
-
 /**
  * Created by Zhang Aqi on 2018/11/30. 15:24
  * mail:1095187377@qq.com
  */
-inline fun <reified T, E> io.reactivex.Observable<T>.bind2Lifecycle(lifecycleProvider: com.trello.rxlifecycle2.LifecycleProvider<E>, disposableObserver: io.reactivex.observers.DisposableObserver<T>): io.reactivex.disposables.Disposable =
-        this.subscribeOn(io.reactivex.schedulers.Schedulers.io())
-                .observeOn(io.reactivex.android.schedulers.AndroidSchedulers.mainThread())
-                .bindToLifecycle(lifecycleProvider)
-                .subscribeWith(disposableObserver)
+inline fun <reified T, E> io.reactivex.Observable<T>.bind2Lifecycle(
+    lifecycleProvider: com.trello.rxlifecycle2.LifecycleProvider<E>,
+    disposableObserver: io.reactivex.observers.DisposableObserver<T>
+): io.reactivex.disposables.Disposable =
+    this.subscribeOn(io.reactivex.schedulers.Schedulers.io())
+        .observeOn(io.reactivex.android.schedulers.AndroidSchedulers.mainThread())
+        .compose(lifecycleProvider.bindToLifecycle())
+        .subscribeWith(disposableObserver)
