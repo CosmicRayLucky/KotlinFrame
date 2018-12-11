c6ab5b93cb54ea4cac6f9253da8eff3c59d8c64b
ZhangAqi <1095187377@qq.com>
2018-12-11 15:57:41 +0800

    添加一些第三方框架util类

---
+++ app/src/main/java/com/example/zaq/kotlinframe/GlobalGlideConfig.kt
@@ -0,0 +1,12 @@
+package com.example.zaq.kotlinframe
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * @author Zhang Aqi
+ * @date 2018/8/27
+ * mail:1095187377@qq.com
+ */
+@GlideModule
+class GlobalGlideConfig : AppGlideModule()
